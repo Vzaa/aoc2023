bfa2feac24e79b982cddf7b7aeca4109a0877ec6
Vzaa <Vzaa@users.noreply.github.com>
2023-12-06 11:18:06 +0300

    Day 06 alternative

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,57 +1,29 @@
 fn p1(instr: &str) -> u64 {
-    let mut iter = instr.lines();
-    let time_str = iter.next().unwrap();
-    let dist_str = iter.next().unwrap();
+    let mut iter = instr.lines().map(|l| {
+        l.split_whitespace()
+            .skip(1)
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<u64>>()
+    });
+    let (times, dists) = (iter.next().unwrap(), iter.next().unwrap());
 
-    let times: Vec<u64> = time_str
-        .split_whitespace()
-        .skip(1)
-        .map(|s| s.parse().unwrap())
-        .collect();
-    let dists: Vec<u64> = dist_str
-        .split_whitespace()
-        .skip(1)
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    let mut mult = 1;
-    for (&t, &d) in times.iter().zip(dists.iter()) {
-        let mut cnt = 0;
-        for i in 0..t {
-            if (t - i) * i > d {
-                cnt += 1;
-            }
-        }
-        mult *= cnt;
-    }
-    mult
+    times
+        .iter()
+        .zip(dists.iter())
+        .map(|(&t, &d)| (0..t).filter(|i| (t - i) * i > d).count() as u64)
+        .product()
 }
 
 fn p2(instr: &str) -> u64 {
-    let mut iter = instr.lines();
-    let time_str = iter.next().unwrap();
-    let dist_str = iter.next().unwrap();
-
-    let t: u64 = time_str
-        .split_whitespace()
-        .skip(1)
-        .fold("".to_owned(), |acc, s| acc + s)
-        .parse()
-        .unwrap();
-    let d: u64 = dist_str
-        .split_whitespace()
-        .skip(1)
-        .fold("".to_owned(), |acc, s| acc + s)
-        .parse()
-        .unwrap();
-
-    let mut cnt = 0;
-    for i in 0..t {
-        if (t - i) * i > d {
-            cnt += 1;
-        }
-    }
-    cnt
+    let mut iter = instr.lines().map(|l| {
+        l.split_whitespace()
+            .skip(1)
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap()
+    });
+    let (t, d) = (iter.next().unwrap(), iter.next().unwrap());
+    (0..t).filter(|i| (t - i) * i > d).count() as u64
 }
 
 fn main() {
