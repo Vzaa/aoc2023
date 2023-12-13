bd62de9ba35905be43f472ea420358926d6e501b
Vzaa <Vzaa@users.noreply.github.com>
2023-12-13 16:46:35 +0300

    Day 12 some clean up

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -37,48 +37,11 @@ impl FromStr for Spring {
 
 fn parse5(s: &str) -> Result<Spring, ()> {
     let (s, r) = s.split_once(' ').unwrap();
-    let tmp = [s, s, s, s, s];
+    let tmp = [s; 5];
     let springs_str = tmp.join("?");
-    let tmp = [r, r, r, r, r];
+    let tmp = [r; 5];
     let rules_str = tmp.join(",");
-    let springs = springs_str.chars().map(char_cond).collect();
-    let rules = rules_str.split(',').map(|n| n.parse().unwrap()).collect();
-    Ok(Spring { springs, rules })
-}
-
-fn get_cmp(springs: &[Cond], expected: &[usize]) -> bool {
-    let mut cnt = 0;
-    let mut idx = 0;
-    for s in springs {
-        match s {
-            Cond::Op => {
-                if cnt != 0 {
-                    if idx >= expected.len() {
-                        return false;
-                    }
-                    if expected[idx] != cnt {
-                        return false;
-                    }
-                    idx += 1;
-                    cnt = 0;
-                }
-            }
-            Cond::Broken => cnt += 1,
-            Cond::Unknown => unreachable!(),
-        }
-    }
-
-    if cnt != 0 {
-        if idx + 1 != expected.len() {
-            return false;
-        }
-        if expected[idx] != cnt {
-            return false;
-        }
-    } else {
-        return idx == expected.len();
-    }
-    true
+    (springs_str + " " + &rules_str).parse()
 }
 
 fn can_put(slots: &[Cond], idx: usize, len: usize) -> bool {
@@ -100,108 +63,61 @@ fn can_put(slots: &[Cond], idx: usize, len: usize) -> bool {
     }
 }
 
-impl Spring {
-    fn solve2(&self) -> u64 {
-        let unknowns: Vec<_> = self
-            .springs
-            .iter()
-            .enumerate()
-            .filter(|(_, s)| matches!(s, Cond::Unknown))
-            .map(|(i, _)| i)
-            .collect();
-
-        let mut cnt = 0;
-        let mut copy = self.springs.clone();
-        let tgt: usize = self.rules.iter().sum();
-        let broken = self
-            .springs
-            .iter()
-            .filter(|x| matches!(x, Cond::Broken))
-            .count();
-        for perm in 0..2_u128.pow(unknowns.len() as u32) {
-            if (unknowns.len() - perm.count_ones() as usize) != (tgt - broken) {
-                continue;
-            }
-            for (i, &pos) in unknowns.iter().enumerate() {
-                if (1 << i) & perm != 0 {
-                    copy[pos] = Cond::Op;
-                } else {
-                    copy[pos] = Cond::Broken;
-                }
-            }
-            if get_cmp(&copy, &self.rules) {
-                cnt += 1;
-            }
-        }
-
-        cnt
-    }
-}
-
-fn solve3(
-    conds: &[Cond],
-    rules: &[usize],
+fn solve<'a>(
+    conds: &'a [Cond],
+    rules: &'a [usize],
     depth: usize,
-    memo: &mut HashMap<(Vec<Cond>, Vec<usize>), u64>,
+    memo: &mut HashMap<(&'a [Cond], &'a [usize]), u64>,
 ) -> u64 {
     let mut perms = 0;
     let length = rules[0];
 
-    if let Some(res) = memo.get(&(conds.to_owned(), rules.to_owned())) {
+    if let Some(res) = memo.get(&(conds, rules)) {
         return *res;
     }
 
-    let mut state = true;
     for i in 0..conds.len() {
-        if state && can_put(&conds, i, length) {
+        if can_put(&conds, i, length) {
             if rules.len() == 1 {
-                if i + length + 1 <= conds.len()
+                if !(i + length + 1 <= conds.len()
                     && conds[i + length + 1..]
                         .iter()
-                        .any(|x| matches!(x, Cond::Broken))
+                        .any(|x| matches!(x, Cond::Broken)))
                 {
-                } else {
                     perms += 1;
                 }
-                match conds[i] {
-                    Cond::Op => state = true,
-                    Cond::Broken => break,
-                    Cond::Unknown => state = true,
-                };
+                if matches!(conds[i], Cond::Broken) {
+                    break;
+                }
                 continue;
             }
             if i + length == conds.len() {
-                match conds[i] {
-                    Cond::Op => state = true,
-                    Cond::Broken => break,
-                    Cond::Unknown => state = true,
-                };
                 continue;
             }
-            let mut dis = 1;
-            dis = dis * solve3(&conds[i + length + 1..], &rules[1..], depth + 1, memo);
-            perms += dis;
+            perms += solve(&conds[i + length + 1..], &rules[1..], depth + 1, memo);
         }
-        match conds[i] {
-            Cond::Op => state = true,
-            Cond::Broken => break,
-            Cond::Unknown => state = true,
-        };
+        if matches!(conds[i], Cond::Broken) {
+            break;
+        }
     }
 
-    memo.insert((conds.to_owned(), rules.to_owned()), perms);
+    // let mut memo2 = HashMap::new();
+    // memo2.insert((conds, rules), perms);
+
+    memo.insert((conds, rules), perms);
     perms
 }
 
 fn p1(instr: &str) -> u64 {
     let springs: Vec<Spring> = instr.lines().map(|l| l.parse().unwrap()).collect();
 
-    let mut sum = 0;
-    for s in springs.iter() {
-        sum += s.solve2();
-    }
-
-    sum
+    springs
+        .iter()
+        .map(|s| {
+            let mut memo = HashMap::new();
+            return solve(&s.springs, &s.rules, 0, &mut memo);
+        })
+        .sum()
 }
 
 fn p2(instr: &str) -> u64 {
@@ -209,9 +125,9 @@ fn p2(instr: &str) -> u64 {
 
     springs5
         .iter()
-        .map(|d| {
+        .map(|s| {
             let mut memo = HashMap::new();
-            return solve3(&d.springs, &d.rules, 0, &mut memo);
+            return solve(&s.springs, &s.rules, 0, &mut memo);
         })
         .sum()
 }
