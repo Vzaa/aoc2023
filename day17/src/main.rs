e6606252d66d1434e42befe015ddf6e2a929043f
Vzaa <Vzaa@users.noreply.github.com>
2023-12-17 19:43:47 +0300

    Day 17 optimized yolo

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
-use std::collections::BinaryHeap;
 use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
-type Pos = (i64, i64);
-type TileMap = HashMap<Pos, u32>;
+type Pos = (i16, i16);
+type TileMap = HashMap<Pos, u16>;
 
 fn add(a: Pos, b: Pos) -> Pos {
     (a.0 + b.0, a.1 + b.1)
@@ -13,13 +14,13 @@ fn parse_map(s: &str) -> TileMap {
     let mut map = HashMap::new();
     for (y, line) in s.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
-            map.insert((x as i64, y as i64), c.to_digit(10).unwrap());
+            map.insert((x as i16, y as i16), c.to_digit(10).unwrap() as u16);
         }
     }
     map
 }
 
-fn p1(instr: &str) -> u32 {
+fn p1(instr: &str) -> u16 {
     let map = parse_map(instr);
     let corner = (
         map.keys().map(|p| p.0).max().unwrap(),
@@ -35,28 +36,15 @@ fn p1(instr: &str) -> u32 {
 
     let mut frontier = BinaryHeap::new();
     frontier.push(Reverse(state_init));
-    let mut best = HashMap::new();
+    let mut visited = HashSet::new();
 
     while let Some(Reverse((cost, cpos, ctraveled))) = frontier.pop() {
-        if let Some(&b) = best.get(&(cpos, ctraveled)) {
-            if b < cost {
-                continue;
-            }
-        }
-        best.insert((cpos, ctraveled), cost);
-
         if cpos == corner {
             return cost;
         }
 
-        let mut push_better = |p, t, c| {
-            if let Some(&b) = best.get(&(p, t)) {
-                if c < b {
-                    best.insert((p, t), c);
-                    frontier.push(Reverse((c, p, t)))
-                }
-            } else {
-                best.insert((p, t), c);
+        let mut push_if_unvisited = |p, t, c| {
+            if visited.insert((p, t)) {
                 frontier.push(Reverse((c, p, t)))
             }
         };
@@ -68,40 +56,40 @@ fn p1(instr: &str) -> u32 {
 
         if let Some(heat) = map.get(&l_pos) {
             if ctraveled.0 < 0 && ctraveled.0 > -3 {
-                push_better(l_pos, add(ctraveled, left), heat + cost);
+                push_if_unvisited(l_pos, add(ctraveled, left), heat + cost);
             } else if ctraveled.0 == 0 {
-                push_better(l_pos, left, heat + cost);
+                push_if_unvisited(l_pos, left, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&r_pos) {
             if ctraveled.0 > 0 && ctraveled.0 < 3 {
-                push_better(r_pos, add(ctraveled, right), heat + cost);
+                push_if_unvisited(r_pos, add(ctraveled, right), heat + cost);
             } else if ctraveled.0 == 0 {
-                push_better(r_pos, right, heat + cost);
+                push_if_unvisited(r_pos, right, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&u_pos) {
             if ctraveled.1 < 0 && ctraveled.1 > -3 {
-                push_better(u_pos, add(ctraveled, up), heat + cost);
+                push_if_unvisited(u_pos, add(ctraveled, up), heat + cost);
             } else if ctraveled.1 == 0 {
-                push_better(u_pos, up, heat + cost);
+                push_if_unvisited(u_pos, up, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&d_pos) {
             if ctraveled.1 > 0 && ctraveled.1 < 3 {
-                push_better(d_pos, add(ctraveled, down), heat + cost);
+                push_if_unvisited(d_pos, add(ctraveled, down), heat + cost);
             } else if ctraveled.1 == 0 {
-                push_better(d_pos, down, heat + cost);
+                push_if_unvisited(d_pos, down, heat + cost);
             }
         }
     }
     unreachable!()
 }
 
-fn p2(instr: &str) -> u32 {
+fn p2(instr: &str) -> u16 {
     let map = parse_map(instr);
     let corner = (
         map.keys().map(|p| p.0).max().unwrap(),
@@ -119,28 +107,15 @@ fn p2(instr: &str) -> u32 {
     let mut frontier = BinaryHeap::new();
     frontier.push(Reverse(state_init_a));
     frontier.push(Reverse(state_init_b));
-    let mut best = HashMap::new();
+    let mut visited = HashSet::new();
 
     while let Some(Reverse((cost, cpos, ctraveled))) = frontier.pop() {
-        if let Some(&b) = best.get(&(cpos, ctraveled)) {
-            if b < cost {
-                continue;
-            }
-        }
-        best.insert((cpos, ctraveled), cost);
-
         if cpos == corner && (ctraveled.1 >= 4 || ctraveled.0 >= 4) {
             return cost;
         }
 
-        let mut push_better = |p, t, c| {
-            if let Some(&b) = best.get(&(p, t)) {
-                if c < b {
-                    best.insert((p, t), c);
-                    frontier.push(Reverse((c, p, t)))
-                }
-            } else {
-                best.insert((p, t), c);
+        let mut push_if_unvisited = |p, t, c| {
+            if visited.insert((p, t)) {
                 frontier.push(Reverse((c, p, t)))
             }
         };
@@ -152,33 +127,33 @@ fn p2(instr: &str) -> u32 {
 
         if let Some(heat) = map.get(&l_pos) {
             if ctraveled.0 < 0 && ctraveled.0 > -10 {
-                push_better(l_pos, add(ctraveled, left), heat + cost);
+                push_if_unvisited(l_pos, add(ctraveled, left), heat + cost);
             } else if ctraveled.1.abs() >= 4 {
-                push_better(l_pos, left, heat + cost);
+                push_if_unvisited(l_pos, left, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&r_pos) {
             if ctraveled.0 > 0 && ctraveled.0 < 10 {
-                push_better(r_pos, add(ctraveled, right), heat + cost);
+                push_if_unvisited(r_pos, add(ctraveled, right), heat + cost);
             } else if ctraveled.1.abs() >= 4 {
-                push_better(r_pos, right, heat + cost);
+                push_if_unvisited(r_pos, right, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&u_pos) {
             if ctraveled.1 < 0 && ctraveled.1 > -10 {
-                push_better(u_pos, add(ctraveled, up), heat + cost);
+                push_if_unvisited(u_pos, add(ctraveled, up), heat + cost);
             } else if ctraveled.0.abs() >= 4 {
-                push_better(u_pos, up, heat + cost);
+                push_if_unvisited(u_pos, up, heat + cost);
             }
         }
 
         if let Some(heat) = map.get(&d_pos) {
             if ctraveled.1 > 0 && ctraveled.1 < 10 {
-                push_better(d_pos, add(ctraveled, down), heat + cost);
+                push_if_unvisited(d_pos, add(ctraveled, down), heat + cost);
             } else if ctraveled.0.abs() >= 4 {
-                push_better(d_pos, down, heat + cost);
+                push_if_unvisited(d_pos, down, heat + cost);
             }
         }
     }
